2eeb6657fd730769e4e21a62f4bf9021d26d89ba
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-08-06 22:04:56 +0200

    TES-31 chore: add mock data for the product card

---
+++ src/data/mockData.js
@@ -0,0 +1,61 @@
+export const phones = [
+    {
+      brandName: 'Apple',
+      modelName: 'iPhone 15',
+      availableColors: [
+        { color: 'Black', stockAmount: 50 },
+        { color: 'Pink', stockAmount: 25 },
+        { color: 'Yellow', stockAmount: 10 },
+        { color: 'Blue', stockAmount: 15 }
+      ],
+      shortDescription: 'The latest iPhone model with advanced features and a powerful processor.',
+      pricePerMonth: 30,
+    },
+    {
+      brandName: 'Samsung',
+      modelName: 'Galaxy S23 FE',
+      availableColors: [
+        { color: 'Green', stockAmount: 10 },
+        { color: 'Lavender', stockAmount: 20 },
+        { color: 'White', stockAmount: 15 },
+        { color: 'Black', stockAmount: 5 }
+      ],
+      shortDescription: 'A feature-packed Galaxy smartphone with a sleek design and a high-quality camera.',
+      pricePerMonth: 23,
+    },
+    {
+      brandName: 'Xiaomi',
+      modelName: 'Redmi Note 13',
+      availableColors: [
+        { color: 'Blue', stockAmount: 0 },
+        { color: 'Black', stockAmount: 0 },
+        { color: 'Yellow', stockAmount: 0 }
+      ],
+      shortDescription: 'A budget-friendly smartphone with a large display and a powerful camera.',
+      pricePerMonth: 8,
+    },
+    {
+      brandName: 'Sony',
+      modelName: 'Xperia 10 V',
+      availableColors: [
+        { color: 'Black', stockAmount: 25 },
+        { color: 'White', stockAmount: 20 },
+        { color: 'Lavender', stockAmount: 15 },
+        { color: 'Green', stockAmount: 15 }
+      ],
+      shortDescription: 'A compact and stylish smartphone with a high-quality camera and a long-lasting battery.',
+      pricePerMonth: 9,
+    },
+    {
+      brandName: 'OnePlus',
+      modelName: 'Nord 3',
+      availableColors: [
+        { color: 'Gray', stockAmount: 100 },
+        { color: 'Green', stockAmount: 100 }
+      ],
+      shortDescription: 'A mid-range smartphone with a fast processor, a large display, and a high-quality camera.',
+      pricePerMonth: 15,
+    }
+  ];
+  
+  
