a9eff6e8246153efebfc0adec7492301d8b1f60e
KozerT <kozer.vizastaff@gmail.com>
2024-08-12 01:38:26 +0200

    feat(fonts): Add and configure Helvetica Neue local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,63 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
+import localFont from 'next/font/local';
 
-const inter = Inter({ subsets: ['latin'] });
+const helveticaNeue = localFont({
+  src: [
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-ThinItalic.woff2',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-RegularItalic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/HelveticaNeueCyr-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-helvetica-neue',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,8 +70,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className={`${helveticaNeue.variable} font-sans`}>
+      <body>{children}</body>
     </html>
   );
 }
