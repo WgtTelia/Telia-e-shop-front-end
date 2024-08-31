2a56149748056ef3d065e944839d637da0853101
KozerT <kozer.vizastaff@gmail.com>
2024-08-31 15:46:23 +0200

    feat: add more test cases to check if it renders the correct banner image based on screen size

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -1,17 +1,57 @@
 import { render, screen } from '@testing-library/react';
 import Header, { bannerImages } from './Header';
 
+jest.mock('next/image', () => jest.requireActual('next/image'));
+
 describe('Header component', () => {
-  it('renders without crashing', () => {
-    render(<Header />);
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
-    expect(screen.getByRole('banner')).toBeInTheDocument();
-  });
+  const resizeWindow = (width: number) => {
+    global.innerWidth = width;
+    global.dispatchEvent(new Event('resize'));
+  };
+
+  const checkBannerImage = (
+    width: number,
+    altText: string,
+    className: string
+  ) => {
+    resizeWindow(width);
+    const bannerImage = screen.getByAltText(altText);
+    expect(bannerImage).toBeInTheDocument();
+    expect(bannerImage).toHaveClass(className);
+  };
 
   it('renders the logo', () => {
     render(<Header />);
     const logoImage = screen.getByRole('img', { name: 'logo' });
     expect(logoImage).toBeInTheDocument();
     expect(logoImage).toHaveAttribute('alt', 'logo');
+  });
+
+  it('renders the navigation element', () => {
+    render(<Header />);
+    const navElement = screen.getByRole('navigation');
+    expect(navElement).toBeInTheDocument();
+  });
+
+  it('ensures all banner images are rendered with correct attributes', () => {
+    render(<Header />);
+    bannerImages.forEach((image) => {
+      const imgElement = screen.getByAltText(image.alt);
+      expect(imgElement).toBeInTheDocument();
+      expect(imgElement).toHaveClass(image.classes);
+      expect(imgElement).toHaveAttribute('width', image.width.toString());
+      expect(imgElement).toHaveAttribute('height', image.height.toString());
+    });
+  });
+  it('renders the correct banner image based on screen size', async () => {
+    render(<Header />);
+
+    checkBannerImage(375, 'Banner for small screens', 'block sm:hidden');
+    checkBannerImage(
+      768,
+      'Banner for medium screens',
+      'hidden sm:block md:hidden'
+    );
+    checkBannerImage(1440, 'Banner for large screens', 'hidden md:block');
   });
 });
