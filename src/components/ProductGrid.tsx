8c8914c7d89476243b48644e3adf8b5d56b66aa2
KozerT <kozer.vizastaff@gmail.com>
2024-09-01 17:26:17 +0200

    style: improve layout for the mobile view

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -4,7 +4,7 @@ import { products } from '@/data/mockData';
 
 export const ProductGrid: React.FC = () => {
   return (
-    <div className='flex flex-wrap gap-4'>
+    <div className='flex flex-wrap justify-center gap-4 md:justify-start'>
       {products.map((product: ProductCardProps) => (
         <ProductCard key={product.productId} {...product} />
       ))}
