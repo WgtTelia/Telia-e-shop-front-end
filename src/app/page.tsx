030d7e85607d768eb9ccb23c0ec6928b0bca5b70
KozerT <kozer.vizastaff@gmail.com>
2024-09-02 12:55:15 +0200

    fix: issue related to the node import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { ProductGrid } from '@/components/ProductGrid';
 import { Filters } from '@/components/Filters';
+import HeroSection from '@/components/header/HeroSection';
 import Header from '@/components/header/Header';
-import HeroSection from '@/components/heroSection/HeroSection';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className='p-main-content md:p-main-content-md lg:p-main-content-lg flex min-h-screen w-full flex-col items-center gap-y-10 lg:gap-y-12'>
+      <main className='flex min-h-screen w-full flex-col items-center gap-y-10 p-main-content md:p-main-content-md lg:gap-y-12 lg:p-main-content-lg'>
         <HeroSection
           title='Mobile Phones & Accessories'
           description='Discover the latest mobile phones and accessories to enhance your digital lifestyle. From sleek designs to powerful features, our selection offers something for everyone.'
