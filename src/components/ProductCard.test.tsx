99a5916eda1dbea69c051e73b44d44ced3e47669
KozerT <kozer.vizastaff@gmail.com>
2024-09-06 21:31:17 +0200

    chore: run esLint and Prettier , fix the issues

--- src/components/ProductCard.test.tsx
+++ src/components/ProductCard.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ProductCard } from './ProductCard';
