103bf6592007675093df0946845ae29be071dd4e
WitchDevelops <d.n.wojewska@gmail.com>
2024-10-08 17:10:39 +0200

    TES-73 refactor: dynamic import in product card, change protocol to http due to error

--- src/lib/services/apiClient.ts
+++ src/lib/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { CanceledError } from 'axios';
 
 export default axios.create({
-    baseURL: 'https://henrika.eu-central-1.elasticbeanstalk.com/api/',
+    baseURL: 'http://henrika.eu-central-1.elasticbeanstalk.com/api/',
 });
 
 export { CanceledError };
