74e02ad4534238f3f0dec32318d432b3dc5ce09b
KozerT <kozer.vizastaff@gmail.com>
2024-11-29 23:30:53 +0100

    feat: uodate variables

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+                destination: '/api/:path*',
             },
         ];
     },
