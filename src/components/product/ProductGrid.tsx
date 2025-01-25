90621b3d11561d54458ca3311b86975bb6488025
WitchDevelops <d.n.wojewska@gmail.com>
2025-01-25 20:35:13 +0100

    refactor: ProductGrid to use a data fetching hook and tanstack query

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -1,49 +1,29 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { CanceledError } from '@/lib/services/apiClient';
 import { Error } from '@/components/apiResponseState/Error';
 import { ProductCard } from '@/components/product/ProductCard';
 import { ProductCardSkeleton } from '@/components/product/ProductCardSkeleton';
-import productService from '@/lib/services/productService';
+import { useProductsQuery } from '@/lib/hooks/useProductsQuery';
 
 export const ProductGrid: React.FC = () => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
-    const [products, setProducts] = useState<ProductData[]>([]);
+    const { data: products, isLoading: loading, error } = useProductsQuery();
     const skeletons = [1, 2, 3, 4, 5, 6];
-
-    useEffect(() => {
-        const { request, cancel } =
-            productService.getAll<APIProductData>(false);
-        request
-            .then((response) => {
-                setProducts((response.data as APIProductData).content);
-            })
-            .catch((error) => {
-                if (error instanceof CanceledError) return;
-                setError(error.message);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-
-        return () => cancel();
-    }, []);
 
     return (
         <div
             data-testid='product-grid'
             className='grid grid-cols-card-grid justify-center gap-6'
         >
-            {error && <Error>{error}</Error>}
+            {error && <Error>{error.message}</Error>}
             {loading &&
                 skeletons.map((skeleton) => (
                     <ProductCardSkeleton key={skeleton} />
                 ))}
-            {products.map((product: ProductCardProps) => (
+            {products && products.map((product: ProductCardProps) => (
                 <ProductCard key={product.id} {...product} />
-            ))}
+            ))
+            }
         </div>
     );
 };
