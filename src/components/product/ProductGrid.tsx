56fb0b337e2facabf5deabbbfadb1b94daa055e1
KozerT <kozer.vizastaff@gmail.com>
2025-04-02 23:35:01 +0200

    feat: expose defaultItemsPerPage and implement pagination in ProductGrid component

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -1,15 +1,27 @@
 'use client';
 import { Error } from '@/components/apiResponseState/Error';
 import { NoResults } from '@/components/filters/NoResults';
+import { defaultItemsPerPage } from '@/components/pagination/PaginationGroup';
 import { ProductCard } from '@/components/product/ProductCard';
 import { ProductCardSkeleton } from '@/components/product/ProductCardSkeleton';
 import { useFilteredProductsByStock } from '@/lib/hooks/useFilteredProductsByStock';
 import { useProductsQuery } from '@/lib/hooks/useProductsQuery';
+import { useSearchParams } from 'next/navigation';
 
 export const ProductGrid: React.FC = () => {
     const { isLoading: loading, error } = useProductsQuery();
     const { filteredProducts } = useFilteredProductsByStock();
+    const searchParams = useSearchParams();
+    const currentPage = searchParams.get('page')
+        ? parseInt(searchParams.get('page') as string)
+        : 1;
+    const itemsPerPage = defaultItemsPerPage;
     const skeletons = [1, 2, 3, 4, 5, 6];
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const productsOnCurrentPage =
+        filteredProducts?.slice(startIndex, endIndex) || [];
 
     return (
         <div
@@ -21,10 +33,9 @@ export const ProductGrid: React.FC = () => {
                 skeletons.map((skeleton) => (
                     <ProductCardSkeleton key={skeleton} />
                 ))}
-            {filteredProducts &&
-                filteredProducts.map((product: ProductCardProps) => (
-                    <ProductCard key={product.id} {...product} />
-                ))}
+            {productsOnCurrentPage.map((product: ProductCardProps) => (
+                <ProductCard key={product.id} {...product} />
+            ))}
             {filteredProducts && filteredProducts.length === 0 && (
                 <NoResults
                     message="Oops! We couldn't find any products matching your current filters."
